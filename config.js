ccef8ea2d31880729c86c372b56392c204df9b09
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2019-11-22 17:00:40 +0700

    Typo on settings

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.VERSION     = "v5.1";
 exports.TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA';
 exports.PREFIX      = '.';
-exports.MT          = FALSE;
-exports.MT_TIME     = "23/11/2019 09:00 (GMT+7)";
+exports.MT          = false;
+exports.MT_TIME     = "23/11 09:00 (GMT+7)";
