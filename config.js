e0356ad222271a5786e6bd6589cd5786c6e86973
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2019-11-24 05:32:20 +0700

    Prepare for update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.ENABLE      = true;
+exports.ENABLE      = false;
 exports.VERSION     = "v5.1";
 exports.TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA';
 exports.PREFIX      = '.';
