e2e44955dd0b7ededaa48dfcf993ccb96b394e98
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2019-11-28 07:41:57 +0700

    LOL `exports const` not working

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-export const ENABLE      = true;
-export const VERSION     = "v7.1";
-// export const TOKEN       = 'NDk2MjAxMDE5MDA1NDY4Njcy.DpNLOA.BCenpnAeFgA75Iaj-U1gxlYw7XQ'; // Velskud
-export const TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA'; // Aisha
-export const PREFIX      = '.';
-export const MT          = false;
-export const MT_TIME     = "23/11 09:00 (GMT+7)";
+exports.ENABLE      = true;
+exports.VERSION     = "v7.1";
+// exports.TOKEN       = 'NDk2MjAxMDE5MDA1NDY4Njcy.DpNLOA.BCenpnAeFgA75Iaj-U1gxlYw7XQ'; // Velskud
+exports.TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA'; // Aisha
+exports.PREFIX      = '.';
+exports.MT          = false;
+exports.MT_TIME     = "23/11 09:00 (GMT+7)";
