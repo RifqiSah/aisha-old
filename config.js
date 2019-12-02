be1049a9909d70967faaa91daa6f2c3ed3019148
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2019-12-02 07:56:12 +0700

    enabling bot

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.ENABLE      = false;
+exports.ENABLE      = true;
 exports.VERSION     = "v7.2 (beta)";
 // exports.TOKEN       = 'NDk2MjAxMDE5MDA1NDY4Njcy.DpNLOA.BCenpnAeFgA75Iaj-U1gxlYw7XQ'; // Velskud
 exports.TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA'; // Aisha
