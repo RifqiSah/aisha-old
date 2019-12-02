e0a284c2b76ac36e860ec6d75d3b0ace31fb1a71
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2019-12-02 07:59:51 +0700

    add `shutdown` command

---
+++ commands/shutdown.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "shutdown",
+    desc: "Matikan bot!.",
+    enable: true,
+    regex: false,
+    help: false,
+    role: ['372915947478056960'],
+    aliases: ['turnoff', 'botkill', 'botstop'],
+	usage: '',
+	cooldown: 0,
+    func: async (client, message, args) => {
+        if (message.author.id != "306616861456465924") return message.channel.send("You're not the bot the owner!")
+        try {
+            await message.channel.send("Bot is shutting down ...");
+            process.exit()
+        } catch(e) {
+            message.channel.send(`ERROR: ${e.message}`);
+        }
+    }
+}
