06f22d97aa1fdc01f552a82d76587f0a47cdf10f
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2019-12-02 07:16:15 +0700

    MT

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.ENABLE      = true;
+exports.ENABLE      = false;
 exports.VERSION     = "v7.1";
 // exports.TOKEN       = 'NDk2MjAxMDE5MDA1NDY4Njcy.DpNLOA.BCenpnAeFgA75Iaj-U1gxlYw7XQ'; // Velskud
 exports.TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA'; // Aisha
