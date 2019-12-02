af3fcfe8abfd5e4049981b350c6db98139434134
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2019-12-02 07:40:58 +0700

    modify response message of ping cmd

--- commands/ping.js
+++ commands/ping.js
@@ -9,6 +9,12 @@ module.exports = {
 	usage: '',
 	cooldown: 0,
     func: (client, message, args) => {
-        message.channel.send('Pong! Latency is ' + parseInt(client.bot.ping) + 'ms.');
+        message.channel.send("Memeriksa ...").then(m => {
+            let ping = m.createdTimestamp - message.createdTimestamp;
+            let choices = ["Ini adalah latency saya", "Apakah baik-baik saja? Saya tidak dapat melihat", "Saya berharap ini tidak buruk"];
+            let response = choices[Math.floor(Math.random() * choices.length)];
+    
+            m.edit(`${response}: Bot Latency: \`${ping}\`, API Latency: \`${Math.round(bot.ping)}\``);
+        });
     }
 }
