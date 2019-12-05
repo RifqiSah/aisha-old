a1a5ff2ec06e314944a5b982c68d7d7ee4b9d5e3
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2019-12-05 21:24:48 +0700

    disable bot

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.ENABLE      = true;
+exports.ENABLE      = false;
 exports.VERSION     = "v7.2";
 exports.TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA';
 exports.PREFIX      = '.';
