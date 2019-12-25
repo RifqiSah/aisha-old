524e1a5a8c71646ec76eb8925932eb63f592f292
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2019-12-25 07:49:28 +0700

    add `info` command

---
+++ commands/info.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name: "info",
+    desc: "Melihat info hal-hal yang ada pada Dragon Nest. Info yang tersedia yaitu:\`\`\`- mq\`\`\`",
+    enable: true,
+    regex: false,
+    help: true,
+    role: [],
+    aliases: [],
+	usage: '[jenis info]',
+	cooldown: 0,
+    func: (client, message, args) => {
+        let info = args.join(" ").toLowerCase();
+        let data = [];
+
+        data.push("__**Info Untuk " + info.replace(/^\w/, c => c.toUpperCase()) + "**__\n");
+
+        switch(true) {
+            case /\bmq|mainquest|cp\b/g.test(info):
+                data.push("https://cdn.discordapp.com/attachments/432903741029613569/560061874406686721/unknown.png");
+                break;
+
+            default:
+                data.push(`Info untuk \`${info}\` tidak ditemukan!`);
+                break;
+        }
+
+        data.push(`\nGunakan \`${client.config.PREFIX}help info\` untuk melihat info yang tersedia.`);
+        message.channel.send(data, { split: true });
+    }
+}
