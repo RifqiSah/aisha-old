51a8a345b893db51cd92afe2ff45b70f63274910
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-01-22 07:08:51 +0700

    disabling bot

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.ENABLE      = true;
+exports.ENABLE      = false;
 exports.VERSION     = "v7.6.4";
 exports.TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA';
 exports.PREFIX      = '.';
