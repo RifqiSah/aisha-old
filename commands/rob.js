cc9d15d7146b5fc0174f9f8407a664bc9fbe778c
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-08 17:58:05 +0700

    viola, rob info using API!

--- commands/rob.js
+++ commands/rob.js
@@ -1,3 +1,6 @@
+const superagent = require("superagent");
+const moment = require('moment');
+
 module.exports = {
     name: "rob",
     desc: "Memberikan informasi untuk `Rock of Blessing` yang sedang ada pada server!",
@@ -10,12 +13,32 @@ module.exports = {
 	cooldown: 0,
     func: (client, message, args) => {
         let data = [];
+        let msg = await message.channel.send(`Megambil data ...`);
 
-        data.push("**6 Feb 2020**");
-        data.push("15:56 `KeikoReef` (PSY)\n");
-        data.push("2 kemungkiann BOT dan sudah habis, selamat mencari RoB :)");
-        data.push("\nWaktu diatas dalam **GMT+8**.");
+        await superagent.get(`http://dev.alriftech.com/core/dragonnest_rob_json`)
+        .then(res => {
+            let robdata = JSON.parse(res.text);
+            let count = 0;
 
-        message.channel.send(data, { split: true });
+            data.push("**Info Pemilik RoB**\n");
+
+            for (let robh in robdata) {
+                let item = robdata[robh];
+                
+                if (item.name.toString().trim() !== "BOT")
+                    data.push(`[${moment(item.date).format("DD-MMM-YYYY")}] [${item.time}] : \`${item.name}\` (${item.class})`);
+                else
+                    count++;
+            }
+
+            data.push(`\n**${count}\** buah RoB mungkin dimiliki oleh BOT atau masih belum ditemukan.`);
+            data.push("Semua waktu diatas dalam **GMT+8**.");
+
+            msg.edit(data);
+        })
+        .catch(err => {
+            msg.edit(`Uh oh, error tidak terduga:\`\`\`${err.status}: ${err.message}\`\`\``).then(msg => {msg.delete(10000)});
+            return;
+        });
     }
 }
--- commands/test.js
+++ commands/test.js
@@ -11,16 +11,6 @@ module.exports = {
 	usage: '',
 	cooldown: 0,
     func: async (client, message, args) => {
-        // message.channel.send('Dalam perbaikan! Tidak ada fitur baru!');
-        
-        let msg = await message.channel.send(`Menunggu \`${args}\` ...`);
-        await superagent.get(`http://dev.alriftech.com/core/dragonnest_rob_json`)
-        .then(res => {
-            msg.edit(`Sukses *grabbing* \`${args}\`! Respon:\`\`\`${res.text}\`\`\``).then(msg => {msg.delete(5000)});
-        })
-        .catch(err => {
-            msg.edit(`Uh oh, error tidak terduga:\`\`\`${err.status}: ${err.message}\`\`\``).then(msg => {msg.delete(10000)});
-            return;
-        });
+        message.channel.send('Dalam perbaikan! Tidak ada fitur baru!');
     }
 }
