f3e2c16324bb88dc8fc80260c3d6188345d03d1f
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-08 15:39:19 +0700

    testing for rob info API

--- commands/test.js
+++ commands/test.js
@@ -9,6 +9,15 @@ module.exports = {
 	usage: '',
 	cooldown: 0,
     func: (client, message, args) => {
-        message.channel.send('Dalam perbaikan! Tidak ada fitur baru!');
+        // message.channel.send('Dalam perbaikan! Tidak ada fitur baru!');
+
+        await superagent.get(`http://dev.alriftech.com/core/dragonnest_rob_json`)
+        .then(res => {
+            msg.edit(`Sukses *grabbing* \`${args}\`! Respon:\`\`\`${res.text}\`\`\``).then(msg => {msg.delete(5000)});
+        })
+        .catch(err => {
+            msg.edit(`Uh oh, error tidak terduga:\`\`\`${err.status}: ${err.message}\`\`\``).then(msg => {msg.delete(10000)});
+            return;
+        });
     }
 }
