b783265c6771ba924940ba9c1a7b4ffeb0f45215
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-08 15:52:03 +0700

    start

--- commands/test.js
+++ commands/test.js
@@ -1,3 +1,5 @@
+const superagent = require("superagent");
+
 module.exports = {
     name: "test",
     desc: "Menguji coba fitur baru pada Aisha.",
@@ -8,9 +10,10 @@ module.exports = {
     aliases: [],
 	usage: '',
 	cooldown: 0,
-    func: (client, message, args) => {
+    func: async (client, message, args) => {
         // message.channel.send('Dalam perbaikan! Tidak ada fitur baru!');
-
+        
+        let msg = await message.channel.send(`Menunggu \`${args}\` ...`);
         await superagent.get(`http://dev.alriftech.com/core/dragonnest_rob_json`)
         .then(res => {
             msg.edit(`Sukses *grabbing* \`${args}\`! Respon:\`\`\`${res.text}\`\`\``).then(msg => {msg.delete(5000)});
