f74cae723308539432fe39a7d9dcdb4c3996ebf5
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-17 06:08:59 +0700

    add allowed assign roles

--- util/funct.js
+++ util/funct.js
@@ -1,5 +1,10 @@
 module.exports = {
     getDate: () => {
         return new Date().toLocaleString("en-US", { timeZone: "Asia/Jakarta" });
+    },
+
+    getAllowedRoles: (roleid) => {
+        let allowedroles = ['668660316036530216', '668680264096022550', '676221506346549251'];
+        return allowedroles.includes(roleid);
     }
 }
