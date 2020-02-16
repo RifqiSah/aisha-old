a2f10bca0627bb67523b27cdcb1c0e87997b781d
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-17 06:08:40 +0700

    add self asign role command

---
+++ commands/iamnot.js
@@ -0,0 +1,33 @@
+let funct = require("../util/funct.js");
+
+module.exports = {
+    name: "iamnot",
+    desc: "Melepas role yang terpasang pada Anda.",
+    enable: true,
+    regex: false,
+    help: false,
+    role: ['372929327903408150', '668439117264191498'],
+    aliases: [],
+	usage: '',
+	cooldown: 0,
+    func: (client, message, args) => {
+        if (!args.length) return message.reply("Kamu tidak memasukan nama role!");
+
+        let role = args.join(" ").toLowerCase();
+        role = role.replace(/\b\w/g, l => l.toUpperCase())
+        role = message.guild.roles.find(r => r.name === role);
+
+        if (funct.getAllowedRoles(role.id)) {
+            if (message.member.roles.has(role.id)) {
+                message.member.removeRole(role.id);
+                message.reply(`Anda berhasil melepaskan role \`${role.name}\`!`);
+            }
+            else {
+                message.reply(`Anda tidak memiliki role \`${role.name}\`!`);
+            }
+        }
+        else {
+            message.reply(`Maaf, role \`${role.name}\` tidak dapat digunakan!`);
+        }
+    }
+}
