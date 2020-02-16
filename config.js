aee685d720dcc43122a3122426d563ff4943d873
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-17 06:50:50 +0700

    disabling bot and increase vresion

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.ENABLE      = true;
-exports.VERSION     = "v1.0.56";
+exports.ENABLE      = false;
+exports.VERSION     = "v1.0.58";
 exports.TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA';
 exports.TOKEN_APIAI = 'dce556d8ef2742a28d6507e5748be7ce';
 exports.PREFIX      = '.';
