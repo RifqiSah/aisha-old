218602f4fcade5ad079032701bdef0923e8e4077
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-02-17 07:12:10 +0700

    display info if noblesse info is not exists

--- commands/noblesse.js
+++ commands/noblesse.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "noblesse",
     desc: "Memberikan informasi untuk `Noblesse Buff` oleh VVIP1!",
     enable: true,
-    regex: true,
+    regex: false,
     help: false,
     role: ['489292018628165633'],
     aliases: ['nb'],
@@ -15,7 +15,7 @@ module.exports = {
         message.delete();
 
         let data = [];
-        let channel = message.guild.channels.find(ch => ch.id === '580444014218838041'); // dragonnest
+        let channel = message.guild.channels.find(ch => ch.id === '678739777700233216'); // noblesse info
         if (!channel) return;
 
         let msg = await channel.send(`Megambil data ...`);
@@ -24,13 +24,16 @@ module.exports = {
         .then(res => {
             let nbdata = JSON.parse(res.text);
 
+            if (Array.isArray(nbdata) && nbdata.length === 0)
+                return msg.edit("Belum ada informasi untuk Noblesse Buff!").then(msg => {msg.delete(10000)});
+
             for (let nbh in nbdata) {
                 let item = nbdata[nbh];
                 
                 data.push("<@&676221506346549251>\n");
                 data.push(`Noblesse Buff akan disebarkan oleh VVIP1 pada tanggal **${moment(item.date).format("DD-MMM-YYYY")}**, pukul **${item.time}** [GMT+8]`);
                 data.push(`<${item.image}>`);
-                // data.push("\nKetikkan `.iam noblesse info` pada `#bot-spam` untuk mendapatkan informasi.")
+                data.push("\nKetik `.iam noblesse info` pada `#bot-spam` untuk mendapatkan informasi.")
             }
 
             msg.edit(data);
