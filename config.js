ee2cf1d79cbb0625bbfa3dd3937b59d1d060fc6c
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-08 08:35:10 +0700

    update: config

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 exports.ENABLE      = true;
 exports.VERSION     = "v1.0.65";
-exports.TOKEN       = 'NDI4NDMyMDcyMzI0MTUzMzQ0.DaILCw.whlk09KLzXEWfvC_XHnPjARfghA';
-exports.TOKEN_APIAI = 'dce556d8ef2742a28d6507e5748be7ce';
 exports.PREFIX      = '.';
 exports.OWNER       = "306616861456465924";
+
+exports.TOKEN       = process.env.TOKEN;
+exports.TOKEN_APIAI = process.env.TOKEN_APIAI;
