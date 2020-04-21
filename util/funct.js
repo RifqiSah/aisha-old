65ca3e25f65780b1be8ff0ef0d71886829f5ed80
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-21 12:17:49 +0700

    add: supported server URL

--- util/funct.js
+++ util/funct.js
@@ -6,5 +6,16 @@ module.exports = {
     getAllowedRoles: (roleid) => {
         let allowedroles = ['668660316036530216', '668680264096022550', '676221506346549251'];
         return allowedroles.includes(roleid);
+    },
+
+    getServerIP: (name) => {
+        let serverIp = [
+            { 'name': 'NA', 'ip': '211.43.155.163', 'port': 14300 },
+            { 'name': 'KO', 'ip': '211.233.18.72', 'port': 14300 },
+            { 'name': 'SEA', 'ip': '202.14.200.67', 'port': 14301 },
+        ];
+
+        let sname = name[0].toUpperCase();
+        return serverIp.find(x => x.name === sname);
     }
 }
