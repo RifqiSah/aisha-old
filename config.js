7802280ed4a1366445609bba0a9e2bec5958e435
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-04-28 13:21:40 +0700

    version bump!

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 exports.ENABLE      = true;
-exports.VERSION     = "v1.1.6";
+exports.VERSION     = "v1.1.11";
 exports.PREFIX      = '.';
 exports.OWNER       = "306616861456465924";
 
