6935f051720a9a0c1ac37efdbc4c3dc10e5a6ef7
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-05-22 07:55:54 +0700

    fix: changelog data

--- commands/bot/changelog.js
+++ commands/bot/changelog.js
@@ -11,17 +11,17 @@ module.exports = {
     usage: '',
     cooldown: 0,
     // eslint-disable-next-line no-unused-vars
-    func: (client, message, args) => {
+    func: async (client, message, args) => {
         const data = [];
 
         data.push(`__**Version ${client.config.VERSION} Changelog**__\n`);
 
         const gh = new Github('');
         // eslint-disable-next-line consistent-return
-        gh.get('repos/RifqiSah/Aisha/commits?per_page=10', (err, res) => {
+        await gh.get('repos/RifqiSah/Aisha/commits?per_page=10', (err, res) => {
             if (err) { return data.push(err); }
 
-            data.push(res.map(res.map((c) => `${c.committer.login} on ${c.commit.committer.date} : ${c.commit.message}`).join('\n')));
+            data.push(res.map((c) => `\`[${c.committer.login}] ${c.commit.committer.date}\`: ${c.commit.message}`).join('\n'));
         });
 
         message.channel.send(data, { split: true });
