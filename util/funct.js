ea589a8bdae8450732db4847f24c0c091f172385
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-05-27 06:53:49 +0700

    add: data getter function

--- util/funct.js
+++ util/funct.js
@@ -1,3 +1,16 @@
+/* eslint-disable array-callback-return */
+const { readFileSync } = require('fs');
+
+function loadData(file) {
+    const rawdata = readFileSync(`./data/${file}`);
+    return JSON.parse(rawdata);
+}
+
+const dndrop = loadData('dndrop.json');
+const dnhp = loadData('dnhp.json');
+const dninfo = loadData('dninfo.json');
+const dnrate = loadData('dnrate.json');
+
 module.exports = {
     getDate: () => new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' }),
 
@@ -14,5 +27,105 @@ module.exports = {
         ];
 
         return serverIp.find((x) => x.name === name);
+    },
+
+    formatData: (name) => {
+        let fmt;
+
+        switch (name) {
+        case 'dndrop':
+            fmt = dndrop.map((item) => '> '.concat(item.name.join(', '))).join('\n');
+            break;
+
+        case 'dnhp':
+            fmt = dnhp.map((item) => '> '.concat(item.name.join(', '))).join('\n');
+            break;
+
+        case 'dninfo':
+            fmt = dninfo.map((item) => '> '.concat(item.name.join(', '))).join('\n');
+            break;
+
+        case 'dnrate':
+            fmt = dnrate.map((item) => '> '.concat(item.name.join(', '))).join('\n');
+            break;
+
+        default:
+            return null;
+        }
+
+        return fmt;
+    },
+
+    // Data
+    getDNDropData: (name) => {
+        const itemData = [];
+        const d = dndrop.find((item) => {
+            const itemReg = new RegExp(item.name.join('|'), 'g');
+            if (!name.match(itemReg)) return null;
+
+            return item;
+        });
+
+        if (!d) return null;
+
+        d.data.map((id) => {
+            itemData.push(id);
+        });
+
+        return itemData;
+    },
+
+    getDNHpData: (name) => {
+        const itemData = [];
+        const d = dnhp.find((item) => {
+            const itemReg = new RegExp(item.name.join('|'), 'g');
+            if (!name.match(itemReg)) return null;
+
+            return item;
+        });
+
+        if (!d) return null;
+
+        d.data.map((id) => {
+            itemData.push(id);
+        });
+
+        return itemData;
+    },
+
+    getDNInfoData: (name) => {
+        const itemData = [];
+        const d = dninfo.find((item) => {
+            const itemReg = new RegExp(item.name.join('|'), 'g');
+            if (!name.match(itemReg)) return null;
+
+            return item;
+        });
+
+        if (!d) return null;
+
+        d.data.map((id) => {
+            itemData.push(id);
+        });
+
+        return itemData;
+    },
+
+    getDNRateData: (name) => {
+        const itemData = [];
+        const d = dnrate.find((item) => {
+            const itemReg = new RegExp(item.name.join('|'), 'g');
+            if (!name.match(itemReg)) return null;
+
+            return item;
+        });
+
+        if (!d) return null;
+
+        d.data.map((id) => {
+            itemData.push(id);
+        });
+
+        return itemData;
     },
 };
