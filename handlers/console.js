646004d2cf39e161f893241b3a08d77cc45d2259
M. Rifqi Saiful A. H <rifqi_sah@yahoo.com>
2020-05-31 08:14:00 +0700

    update: handlers

--- handlers/console.js
+++ handlers/console.js
@@ -4,7 +4,7 @@ module.exports = (client) => {
     const promp = process.openStdin();
     promp.addListener('data', (res) => {
         const x = res.toString().trim().split(/ +/g);
-        client.bot.channels.get('713324629967634502').send(x.join(' ')); // bot-console
+        client.bot.channels.cache.get('713324629967634502').send(x.join(' ')); // bot-console
     });
 
     console.log('  [V] Done!');
